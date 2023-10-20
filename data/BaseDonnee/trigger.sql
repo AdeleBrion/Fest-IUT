568cb1197ea76d365907bb6d809d413a6e6b5895
Daniel Moreira <daniel.moreira.41160@gmail.com>
2023-10-20 12:21:54 +0200

    Ajout Trigger Habitation Plein

--- data/BaseDonnee/trigger.sql
+++ data/BaseDonnee/trigger.sql
@@ -12,3 +12,18 @@ BEGIN
     end if;
 END |
 DELIMITER ;
+
+DELIMITER|
+CREATE OR REPLACE TRIGGER HabitationPlein BEFORE INSERT ON ACCUEILIR FOR EACH ROW
+BEGIN
+    declare nbPerson int;
+    declare nbMax int;
+    declare mes varchar(100);
+    select nbMax into nbMax from HEBERGEMENT where idHeb=new.idHeb;
+    select sum(nbPersonne) into nbPerson from ACCUEILIR where idHeb=new.idHeb and dateheureHeb=new.dateheure_heb;
+    if nbPerson+new.nbPersonne > nbMax then
+        set mes = concat('L hebergement ', new.idHeb, ' ne peut pas accépté autant de personne');
+        signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
+    end if;
+END |
+DELIMITER ;
