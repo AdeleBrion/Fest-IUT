883ddc6cacdcd58e7747e86d480850d730bb2a7d
Daniel Moreira <daniel.moreira.41160@gmail.com>
2023-12-01 14:29:17 +0100

    Test de déploment sur site web

--- site_web/views.py
+++ site_web/views.py
@@ -1,8 +1,13 @@
-from .app import app
+from .app import app, db
+from .models import Lieu, Spectateur, Concert, Style, SousStyle, Billet, GroupeMusical
 from flask import render_template
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    concert = Concert.query.all()
+    return render_template('home.html', Concerts = concert)
 
+@app.route('/index/')
+def index():
+    return render_template('index.html')
