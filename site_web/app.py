abb0d0613b2a32dad2cccdcbdb0ae185657fee7e
Sullivaniut <demaret.sullivan@yahoo.com>
2023-12-05 17:20:22 +0100

    Ajout de l'implémentation du bouton mon compte avec la redirection

--- site_web/app.py
+++ site_web/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 import os.path
 
 def mkpath(p):
@@ -8,5 +9,9 @@ def mkpath(p):
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///'+mkpath('../data/BaseDonnee/BD_Festiut.db'))
 app.config['SECRET_KEY'] = 'e8ca6d1f-c135-412b-95d0-35768946d93c'
+
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
 db = SQLAlchemy(app)
--- site_web/views.py
+++ site_web/views.py
@@ -1,15 +1,20 @@
-from .app import app
+from .app import app, login_manager
 from flask import render_template, redirect, url_for, request
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from .models import get_email_spectateur
+from .models import get_email_spectateur, Spectateur
 from hashlib import sha256
 
 @app.route('/')
 def home():
     return render_template('accueil.html')
 
+
+
+#-----------------------------------------------------#
+#                   Connexion                         #
+#-----------------------------------------------------#
 
 class LoginForm(FlaskForm):
     identifiant = StringField('Identifiant')
@@ -35,6 +40,7 @@ def login():
     if f.validate_on_submit():
         util = f.get_authentification_utilisateur()
         if util:
+            login_user(util)
             return redirect(url_for("home"))
         else:
             return render_template(
@@ -50,3 +56,16 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@login_manager.user_loader
+def load_user(user):
+    return Spectateur.query.get(int(user))
+
+#-----------------------------------------------------#
+#                   Compte                            #
+#-----------------------------------------------------#
+
+@app.route('/compte')
+@login_required
+def compte():
+    return render_template('compte.html')
